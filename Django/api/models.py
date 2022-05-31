83839964bd7d7b60d820aec5f27b803b6c018c30
SpecTr03 <sebastian.cifuentes0808@gmail.com>
2022-05-30 20:22:29 -0500

    Agregando Models

--- Django/api/models.py
+++ Django/api/models.py
@@ -1,3 +1,27 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+
+class Usuario (models.Model):
+
+    sexo = [
+        ('M','Masculino'),
+        ('F','Femenino'),
+    ]
+
+    nombre = models.CharField("Nombre", max_length=30)
+    apellidos = models.CharField("Apellidos", max_length=50)
+    nombre_usuario = models.CharField("Nombre Usuario", max_length=20)
+    contrasenia = models.CharField("Contrasenia", max_length=100)
+    sexo = models.CharField("Sexo", choices=sexo)
+    correo_electronico = models.EmailField("Correo Electronico", max_length=50)
+    foto_perfil = models.ImageField("Foto De Perfil", blank=True)
+    edad = models.PositiveIntegerField("Edad")
+
+
+
+class Resenia(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    calificacion = models.IntegerField("Calificacion",max_length=1)
+    comentario = models.TextField("Comentario")
