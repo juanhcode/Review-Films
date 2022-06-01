90da5da39c5f7dafd92686f7c1baa565a93ee70e
Juan Hoyos <juanhoyos1347@gmail.com>
2022-06-01 11:09:18 -0500

    modelo de rutas creado

---
+++ client_review_films/lib/models/menu_ruta.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class MenuRuta {
+  final String route;
+  final String name;
+  final Widget screen;
+
+  MenuRuta({required this.route, required this.name, required this.screen});
+}
