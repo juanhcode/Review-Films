1f8ee08e5af39e1ce532f2ab23ba902f7814a05e
Juan Hoyos <juanhoyos1347@gmail.com>
2022-06-11 22:51:47 -0500

    eidgets archivo

---
+++ client_review_films/lib/widgets/widgets.dart
@@ -0,0 +1,3 @@
+export 'package:client_review_films/widgets/bottom_custom.dart';
+export 'package:client_review_films/widgets/movie_slider.dart';
+export 'package:client_review_films/widgets/card_swiper.dart';
