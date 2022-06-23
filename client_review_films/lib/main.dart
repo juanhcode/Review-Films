a2838da06aeff4cf9ec107d24725b2763a297e24
Juan Hoyos <juanhoyos1347@gmail.com>
2022-06-23 17:08:29 -0500

    se agrego el gestor de estados

--- client_review_films/lib/main.dart
+++ client_review_films/lib/main.dart
@@ -1,8 +1,22 @@
+import 'package:client_review_films/providers/movies_provider.dart';
 import 'package:client_review_films/router/app_router.dart';
 import 'package:client_review_films/theme/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(const AppState());
+
+class AppState extends StatelessWidget {
+  const AppState({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => MoviesProvider(),lazy:false)],
+      child: const MyApp(),
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
